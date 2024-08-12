4521612908c0cb962d81e4b55872d5fe12130e21
Sharat Madanapalli <sharat@canopusnet.com>
2024-08-12 16:01:41 +1000

    db added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/4033f3b2-0fc9-4a6e-95be-37009f50e078-7qajqq.png",
@@ -12,7 +13,10 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
 
@@ -21,6 +25,13 @@ export default function HomePage() {
       </div>
 
       <div className="flex flex-wrap gap-4">
+        {
+          posts.map((post) => (
+            <div key={post.id} className="w-48">
+              {post.name}
+            </div>
+          ))
+        }
         {
           mockImages.map((image) => (
             <div key={image.id} className="w-48">
