c52cd0b28edfd5a48e9f9994c7c6ad794e316afa
Sharat Madanapalli <sharat@canopusnet.com>
2024-08-12 15:26:53 +1000

    nav and perf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const mockImages = mockUrls.map((url, index) => ({
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
 
       <div className="text-3xl font-bold mb-8">
         Hello (gallery in progress!)
